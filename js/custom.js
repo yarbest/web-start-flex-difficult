ab4754a9867703b1931baa82483784d6bacc54fd
yarbest <72338354+yarbest@users.noreply.github.com>
2020-10-17 21:02:26 +0300

    Слайдер

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+jQuery(function ($) {
+    $(document).ready(function () {
+        $(".bxslider").bxSlider({
+            // slideWidth: "400",
+            // slideMargin: "200",
+        });
+    });
+});
