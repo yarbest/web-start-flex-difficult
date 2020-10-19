dd1ae453a1fc400f6663809358281c37c71ff545
yarbest <72338354+yarbest@users.noreply.github.com>
2020-10-19 13:27:23 +0300

    z

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,14 @@
-jQuery(function ($) {
-    $(document).ready(function () {
-        $(".bxslider").bxSlider({
-            // slideWidth: "400",
-            // slideMargin: "200",
-        });
+// jQuery(function ($) {
+//     $(document).ready(function () {
+//         $(".bxslider").bxSlider({
+//             // slideWidth: "400",
+//             // slideMargin: "200",
+//         });
+//     });
+// });
+
+$(document).ready(function () {
+    $(".slider").slick({
+        dots: true,
     });
 });
